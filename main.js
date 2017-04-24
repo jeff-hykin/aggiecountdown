55f709760b82ac3e24836311643d6cfc2eb429c0
Rick Shaw <rick@The-Senate.local>
2017-04-24 13:45:18 -0500

    Removed Some Naughty Colors

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 var schedule = [[], [], [], [], [], [], []];
 
-const colors = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green', 'light-green', 'lime', 'amber', 'orange', 'deep-orange', 'brown', 'grey', 'blue-grey'];
+const colors = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'cyan', 'teal', 'green', 'light-green', 'lime', 'amber', 'orange', 'deep-orange', 'brown', 'blue-grey'];
 
 $(document).ready(function() {
   $('#addActivity').click(function() {
